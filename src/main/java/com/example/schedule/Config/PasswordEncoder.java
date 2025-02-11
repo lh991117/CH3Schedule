63ba4c380f73ca5f46b1807e9a3bfffdf4f485c0
lh991117 <92137419+lh991117@users.noreply.github.com>
2025-02-11 21:47:07 +0900

    feat : valid, password dev

---
+++ src/main/java/com/example/schedule/Config/PasswordEncoder.java
@@ -0,0 +1,16 @@
+package com.example.schedule.Config;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PasswordEncoder {
+    public String encode(String password) {
+        return BCrypt.withDefaults().hashToString(BCrypt.MIN_COST, password.toCharArray());
+    }
+
+    public boolean matches(String rawPassword, String encodedPassword) {
+        BCrypt.Result result = BCrypt.verifyer().verify(rawPassword.toCharArray(), encodedPassword.toCharArray());
+        return result.verified;
+    }
+}
--- src/main/java/com/example/schedule/Dto/CreateScheduleRequestDto.java
+++ src/main/java/com/example/schedule/Dto/CreateScheduleRequestDto.java
@@ -1,10 +1,14 @@
 package com.example.schedule.Dto;
 
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class CreateScheduleRequestDto {
+    //할일 제목 글자를 10까지 설정
+    @Size(min = 1, max = 10)
     private final String todoTitle;
+    //할일 내용은 사이즈 미정(길이가 길 수 있기 때문에)
     private final String todoContent;
     private final String username;
 
